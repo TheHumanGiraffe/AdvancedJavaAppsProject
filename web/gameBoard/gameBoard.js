21845e4bd90f8cf54a39220b6937f9c59cbc536a
Micheal Walburn <michealwalburn@protonmail.com>
2019-11-19 15:40:41 -0600

    Front end changes
    shows entire table now when four people enter the same room, centered
    the contents so that it looks nice, more felt image.

--- web/gameBoard/gameBoard.js
+++ web/gameBoard/gameBoard.js
@@ -3,53 +3,10 @@
 	
 	var cardURL = 'https://raw.githubusercontent.com/TheHumanGiraffe/AdvancedJavaAppsProject/master/web/gameBoard/cards/';
 	
-	/*$.getJSON('../../inbound.json', function(data) {
-		var container = document.getElementById('discard');
-		var img = document.createElement('img');
-			img.src = cardURL + data.topDiscard.CardID + '.jpg'; // img[i] refers to the current URL.
-			img.title = data.topDiscard.Name; 
-			container.appendChild(img);
-		$.each(data.table, function (key, val) {
-			var container = document.getElementById('table');
-			var img = document.createElement('img');
-				img.src = 'https://raw.githubusercontent.com/TheHumanGiraffe/AdvancedJavaAppsProject/master/cards/' + val.CardID + '.jpg'; // img[i] refers to the current URL.
-				img.title = val.Name;
-				container.appendChild(img);
-		});
-		$.each(data.player1.hand, function (key, val) {
-			var container = document.getElementById('player1');
-			var img = document.createElement('img');
-				img.src = 'https://raw.githubusercontent.com/TheHumanGiraffe/AdvancedJavaAppsProject/master/cards/' + val.CardID + '.jpg'; // img[i] refers to the current URL.
-				img.title = val.Name;
-				container.appendChild(img);
-		});
-		$.each(data.player2.hand, function (key, val) {
-			var container = document.getElementById('player2');
-			var img = document.createElement('img');
-				img.src = 'https://raw.githubusercontent.com/TheHumanGiraffe/AdvancedJavaAppsProject/master/cards/' + val.CardID + '.jpg'; // img[i] refers to the current URL.
-				img.title = val.Name;
-				container.appendChild(img);
-		});
-		$.each(data.player3.hand, function (key, val) {
-			var container = document.getElementById('player3');
-			var img = document.createElement('img');
-				img.src = 'https://raw.githubusercontent.com/TheHumanGiraffe/AdvancedJavaAppsProject/master/cards/' + val.CardID + '.jpg'; // img[i] refers to the current URL.
-				img.title = val.Name;
-				container.appendChild(img);
-		});
-		$.each(data.player4.hand, function (key, val) {
-			var container = document.getElementById('player4');
-			var img = document.createElement('img');
-				img.src = 'https://raw.githubusercontent.com/TheHumanGiraffe/AdvancedJavaAppsProject/master/cards/' + val.CardID + '.jpg'; // img[i] refers to the current URL.
-				img.title = val.Name;
-				container.appendChild(img);
-		});
-	});*/
+	$(document).ready(function() {
+		$("#echoText").val('');
+	});
 	
-	
-	//var json = JSON.parse(jsonText); 
-	
-	//
 	if(urlParams.has('roomNumber')){
 		var roomNumber = urlParams.get('roomNumber')
 	}else{
@@ -99,21 +56,28 @@
         webSocket.close();
     }
     function wsGetMessage(message){
-    	console.log(message);
-    	console.log(message.data);
+    	document.getElementById("player1Div").innerHTML = "";
+    	document.getElementById("player2Div").innerHTML = "";
+    	document.getElementById("player3Div").innerHTML = "";
+    	document.getElementById("player4Div").innerHTML = "";
+    	
+    	console.dir(message);
+    	console.dir(message.data);
     	
     	var json = JSON.parse(message.data);
     	
-    	console.log(json);
+    	console.dir(json);
     	
     	var gamestate = json.gamestate;
     	
     	var currentPlayer = gamestate.players[gamestate.visible];
+    	console.dir(gamestate);
+    	console.dir(currentPlayer);
+    	console.dir(currentPlayer.hand);
     	
-    	console.log(currentPlayer);
-    	console.log(currentPlayer.hand);
+    	
     	if(currentPlayer.hand.length>0) {
-    		console.log(currentPlayer.hand[0]);
+    		console.dir(currentPlayer.hand[0]);
     		console.log(currentPlayer.hand[0].cardID);
     	} else {
     		echoText.value += "Waiting for players...\n";
@@ -124,14 +88,44 @@
     	
     	document.getElementById("discard").src = cardURL + gamestate.deck.discards[0].cardID + '.jpg';
     	
-    	var player1Cards = document.getElementById("player1Table");
-    	player1Cards.deleteRow(0);
-    	var cardRow = player1Cards.insertRow(0);
+//    	var player1Cards = document.getElementById("player1Table");
+//    	player1Cards.deleteRow(0);
+//    	var cardRow = player1Cards.insertRow(0);
     	
-    	currentPlayer.hand.forEach(function(card){
-    		var x=cardRow.insertCell(-1);
-            x.innerHTML ="<img class=\"p1\" src='"+cardURL + card.cardID + ".jpg'"+"/>";
+//    	currentPlayer.hand.forEach(function(card){
+//    		var x=cardRow.insertCell(-1);
+//            x.innerHTML ="<img class=\"p1\" src='"+cardURL + card.cardID + ".jpg'"+"/>";
+//    	});
+    	
+    	var player1div = document.getElementById('player1Div');
+    	var player2div = document.getElementById('player2Div');
+    	var player3div = document.getElementById('player3Div');
+    	var player4div = document.getElementById('player4Div');
+    	
+    	currentPlayer.hand.forEach(function(card) {
+    		var img = document.createElement("img");
+    		img.src = cardURL + card.cardID + ".jpg";
+    		img.className = "p1";
+    		player1div.appendChild(img);
+    	})
+    	
+    	players.splice(gamestate.visible, 1);
+    
+    	var iterator = 2;
+    	players.forEach(function(player) {
+    		console.log(player);
+    		
+    		var workingDiv = document.getElementById('player' + iterator + 'Div');
+    		
+    		player.hand.forEach(function(card){
+    			var img = document.createElement("img");
+    			img.src = cardURL + card.cardID + ".jpg";
+    			img.className = "p" + iterator;
+    			workingDiv.appendChild(img);
+     		});
+    		iterator++;
     	});
+    	
     	
     	/*var player2Cards = document.getElementById("player2Table");
     	
